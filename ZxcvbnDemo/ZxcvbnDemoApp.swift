fa38ac3ae4fbc2eee9f27ae5b3bc81e5c5e5a66a
Julien Eyriès <julien@eyries.fr>
2022-11-23 21:30:30 +0100

    Initial Commit

---
+++ ZxcvbnDemo/ZxcvbnDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZxcvbnDemoApp.swift
+//  ZxcvbnDemo
+//
+//  Created by Julien on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ZxcvbnDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
