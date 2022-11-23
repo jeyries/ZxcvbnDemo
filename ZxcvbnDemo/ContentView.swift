bdd03cf7b62fb654619ef2be967400cb3f10deaf
Julien Eyriès <julien@eyries.fr>
2022-11-23 21:48:23 +0100

    Added ZxcvbnC & ZxcvbnJS

--- ZxcvbnDemo/ContentView.swift
+++ ZxcvbnDemo/ContentView.swift
@@ -6,17 +6,47 @@
 //
 
 import SwiftUI
+import ZxcvbnCWrapper
+import ZxcvbnJS
+
+let zxcvbnC = ZxcvbnCWrapper.shared
+let zxcvbnJS = ZxcvbnJS()
 
 struct ContentView: View {
+    
+    @State var password = "Tr0ub4dour&3"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section("Input") {
+                    TextField("password", text: $password)
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                }
+                
+                Section("ZxcvbnC") {
+                    LabeledContent("entropy", value: "\(resultC.entropy)")
+                }
+                
+                Section("ZxcvbnJS") {
+                    LabeledContent("score", value: "\(resultJS.score)")
+                    LabeledContent("warning", value: "\(resultJS.warning)")
+                    LabeledContent("suggestions", value: "\(resultJS.suggestions)")
+                }
+            }
+            .navigationTitle("Zxcvbn Demo")
         }
-        .padding()
     }
+    
+    var resultC: ZxcvbnCWrapper.Result {
+        return zxcvbnC.evaluate(password: password)
+    }
+
+    var resultJS: ZxcvbnJS.Result {
+        return zxcvbnJS.evaluate(password: password)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
